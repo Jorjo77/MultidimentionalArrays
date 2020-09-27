ff40224bc20fc4d8c6188fd72dea74e6a58139bd
Jorjo77 <61197544+Jorjo77@users.noreply.github.com>
2020-09-27 23:11:55 +0300

    Tasks update

---
+++ KnightGame/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] chessPlayground = ReadMatrix(size, size);
+            int removementsCounter = 0;
+            int maxAttacks = 0;
+            int rowMaxAttaks = 0;
+            int colMaxAttaks = 0;
+
+            while (true)
+            {
+
+                bool IsAttaced = false;
+                for (int row = 0; row < chessPlayground.GetLength(0); row++)
+                {
+                    for (int col = 0; col < chessPlayground.GetLength(1); col++)
+                    {
+                        if (chessPlayground[row, col] == 'K')
+                        {
+                            int attacksCounter = 0;
+                            if (IsInside(chessPlayground, row - 1, col - 2) && chessPlayground[row - 1, col - 2] == 'K')
+                            {
+                                attacksCounter++;
+                                IsAttaced = true;
+                            }
+                            if (IsInside(chessPlayground, row - 2, col - 1) && chessPlayground[row - 2, col - 1] == 'K')
+                            {
+                                attacksCounter++;
+                                IsAttaced = true;
+                            }
+                            if (IsInside(chessPlayground, row + 1, col + 2) && chessPlayground[row + 1, col + 2] == 'K')
+                            {
+                                attacksCounter++;
+                                IsAttaced = true;
+                            }
+                            if (IsInside(chessPlayground, row + 1, col - 2) && chessPlayground[row + 1, col - 2] == 'K')
+                            {
+                                attacksCounter++;
+                                IsAttaced = true;
+                            }
+                            if (IsInside(chessPlayground, row + 2, col + 1) && chessPlayground[row + 2, col + 1] == 'K')
+                            {
+                                attacksCounter++;
+                                IsAttaced = true;
+                            }
+                            if (IsInside(chessPlayground, row - 2, col - 1) && chessPlayground[row - 2, col - 1] == 'K')
+                            {
+                                attacksCounter++;
+                                IsAttaced = true;
+                            }
+                            if (IsInside(chessPlayground, row - 1, col + 2) && chessPlayground[row - 1, col + 2] == 'K')
+                            {
+                                attacksCounter++;
+                                IsAttaced = true;
+                            }
+                            if (IsInside(chessPlayground, row - 2, col + 1) && chessPlayground[row - 2, col + 1] == 'K')
+                            {
+                                attacksCounter++;
+                                IsAttaced = true;
+                            }
+
+                            if (attacksCounter > maxAttacks)
+                            {
+                                maxAttacks = attacksCounter;
+                                rowMaxAttaks = row;
+                                colMaxAttaks = col;
+                            }
+                        }
+                    }
+                }
+                if (IsAttaced)
+                {
+                    chessPlayground[rowMaxAttaks, colMaxAttaks] = '0';
+                    removementsCounter++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(removementsCounter);
+        }
+        private static char[,] ReadMatrix(int rows, int cols)
+        {
+            char[,] matrix = new char[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] rowData = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            return matrix;
+        }
+        private static bool IsInside(char[,] matrix, int targetRow, int targetCol)
+        {
+            return targetRow >= 0 && targetCol >= 0 && targetRow < matrix.GetLength(0) && targetCol < matrix.GetLength(1);
+        }
+    }
+}
---
+++ SnakeMoves/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse).ToArray();
+            int rows = size[0];
+            int cols = size[1];
+            char[,] matrix = new char[rows, cols];
+            string snakeName = Console.ReadLine();
+            int count = 0;
+            char[] splittedSnake = snakeName.ToCharArray();
+            //char currSymbol = splittedSnake[count];!? не ставаше с currSymbol - не се променяше, а със splittedSnake[count]; работи!?
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (row%2==0)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        matrix[row, col] = splittedSnake[count];
+                        count++;
+                        if (count == splittedSnake.Length)
+                        {
+                            count = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = splittedSnake[count];
+                        count++;
+                        if (count == splittedSnake.Length)
+                        {
+                            count = 0;
+                        }
+                    }
+                }
+            }
+            PrintMatrix(matrix);
+        }
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
