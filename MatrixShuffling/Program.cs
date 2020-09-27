7c98e369e8a271536a099269bdec2de4271cf5da
Jorjo77 <61197544+Jorjo77@users.noreply.github.com>
2020-09-27 07:47:28 +0300

    Update tasks

---
+++ MatrixShuffling/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace MatrixShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            int row = sizes[0];
+            int col = sizes[1];
+            string[,] matrix = ReadMatrix(row, col);
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command=="END")
+                {
+                    break;
+                }
+
+                if (!IsValidCommand(command, row, col))
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else
+                {
+                    string[] commands = command.Split().ToArray();
+                    int row1 = int.Parse(commands[1]);
+                    int col1 = int.Parse(commands[2]);
+                    int row2 = int.Parse(commands[3]);
+                    int col2 = int.Parse(commands[4]);
+                    string temp = matrix[row2, col2];
+                    matrix[row2, col2] = matrix[row1, col1];
+                    matrix[row1, col1] = temp;
+
+                    PrintMatrix(matrix);
+                }
+            }
+        }
+
+        private static string[,] ReadMatrix(int rows, int cols)
+        {
+            string[,] matrix = new string[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] rowData = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            return matrix;
+        }
+
+        private static bool IsValidCommand(string command, int row, int col)
+        {
+            string[] commands = command.Split().ToArray();
+
+            if (commands.Length==5&& commands[0] == "swap" && int.Parse(commands[1]) <= row&& int.Parse(commands[2]) <= col
+                && int.Parse(commands[3]) <= row && int.Parse(commands[4]) <= col)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void PrintMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- MaximalSum/Program.cs
+++ MaximalSum/Program.cs
@@ -12,32 +12,32 @@ namespace MaximalSum
                 .Split(" ")
                 .Select(int.Parse).ToArray();
             int[,] matrix = ReadMatrix(size[0], size[1]);
-            int sum = 0;
+            int winRow = 0;
+            int winCol = 0;
             int maxSum = int.MinValue;
-
-            for (int row = 0; row < matrix.GetLength(0); row++)
+            int[,] matrixWinner = new int[3, 3];
+            for (int row = 0; row < matrix.GetLength(0)-2; row++)
             {
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                for (int col = 0; col < matrix.GetLength(1)-2; col++)
                 {
-                    if (row >= 2 && col >= 2)
-                    {
-                        sum += matrix[row, col];
-                        sum += matrix[row, col - 1];
-                        sum += matrix[row, col - 2];
-                        sum += matrix[row - 1, col];
-                        sum += matrix[row - 1, col - 1];
-                        sum += matrix[row - 1, col - 2];
-                        sum += matrix[row - 2, col];
-                        sum += matrix[row - 2, col - 1];
-                        sum += matrix[row - 2, col - 2];
+                        int sum = 0;
+
+                        sum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] + matrix[row+1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
                         if (sum>maxSum)
                         {
                             maxSum = sum;
+                            winRow = row;
+                            winCol = col;
                         }
-                    }
                 }
             }
-            Console.WriteLine(maxSum);
+            Console.WriteLine($"Sum = {maxSum}");
+            Console.Write(matrix[winRow, winCol] + " " + matrix[winRow, winCol +1] + " " + matrix[winRow, winCol + 2]);
+            Console.WriteLine();
+            Console.Write(matrix[winRow + 1, winCol] + " " + matrix[winRow + 1, winCol + 1] + " " + matrix[winRow + 1, winCol + 2]);
+            Console.WriteLine();
+            Console.Write(matrix[winRow + 2, winCol] + " " + matrix[winRow + 2, winCol + 1] + " " + matrix[winRow + 2, winCol + 2]);
         }
         private static int[,] ReadMatrix(int rows, int cols)
         {
